e90ecaa98ac00d89eba1fe8995d011d33fc0f9c9
andres <anagudelogu@gmail.com>
2024-07-24 10:52:16 -0500

    Improve SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,63 @@ import './globals.css';
 import Navbar from '@/components/Navbar';
 import Drawer from '@/components/Drawer';
 import DrawerContextProvider from '@/contexts/DrawerContext';
-import ThemeContextProvider, { useThemeContext } from '@/contexts/ThemeContext';
+import ThemeContextProvider from '@/contexts/ThemeContext';
 
 const rubik = Rubik({ subsets: ['latin'], variable: '--font-rubik' });
 const syne = Syne({ subsets: ['latin'], variable: '--font-syne' });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://andresagudelo.dev'),
   title: 'Andres Agudelo | Frontend Developer',
   description:
     'Frontend Developer with years of experience, passionate about crafting user-friendly interfaces and exploring cutting-edge web technologies.',
+  keywords: [
+    'Frontend Developer',
+    'React',
+    'Next.js',
+    'Tailwind CSS',
+    'TypeScript',
+  ],
+  openGraph: {
+    siteName: 'Andres Agudelo | Frontend Developer',
+    type: 'website',
+    locale: 'en_US',
+    description:
+      'Frontend Developer with years of experience, passionate about crafting user-friendly interfaces and exploring cutting-edge web technologies.',
+    images: [
+      {
+        url: 'https://andresagudelo.dev/about-me.png',
+        width: 1200,
+        height: 630,
+        alt: 'Andres Agudelo | Frontend Developer',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Andres Agudelo | Frontend Developer',
+    description:
+      'Frontend Developer with years of experience, passionate about crafting user-friendly interfaces and exploring cutting-edge web technologies.',
+    images: [
+      {
+        url: 'https://andresagudelo.dev/about-me.png',
+        width: 1200,
+        height: 630,
+        alt: 'Andres Agudelo | Frontend Developer',
+      },
+    ],
+  },
+  alternates: {
+    canonical: 'https://andresagudelo.dev',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    'max-image-preview': 'large',
+    'max-snippet': -1,
+    'max-video-preview': -1,
+    googleBot: 'index, follow',
+  },
 };
 
 export default function RootLayout({
