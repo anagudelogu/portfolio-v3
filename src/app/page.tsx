1f5bc4663dd890c9ab98c6b686a87a66b1fa804e
andres <anagudelogu@gmail.com>
2024-09-03 17:31:25 -0500

    Add about-me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,12 @@ import TechStackSlider from '@/components/tech-stack-slider'
 import { Button } from '@/components/ui/button'
 import { ArrowDownIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
+import personalPic from '@/assets/images/personal-pic.png'
+import amsterdamPic from '@/assets/images/amsterdam.jpeg'
+import ghentPic from '@/assets/images/ghent.png'
+import ibizaPic from '@/assets/images/ibiza.png'
+import map from '@/assets/images/map.jpeg'
+import memojiThumb from '@/assets/images/memoji-thumbsup.webp'
 
 export default function Home() {
   return (
@@ -100,9 +106,76 @@ export default function Home() {
         </div>
       </section>
 
-      <div className="mx-auto mb-32 max-w-6xl overflow-x-clip py-16 md:py-32">
+      <div className="mx-auto max-w-6xl overflow-x-clip py-16 md:py-32">
         <TechStackSlider />
       </div>
+
+      <section className="container mb-32">
+        <div className="flex justify-center">
+          <h3 className="gradient-text font-serif text-sm font-bold uppercase">Beyond the code</h3>
+        </div>
+        <h2 className="mb-10 mt-6 text-center text-3xl font-extrabold tracking-wide md:text-5xl">
+          About me
+        </h2>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          <article className="relative mx-auto max-h-[350px] w-full rounded-2xl bg-gray-400/15">
+            <Image src={personalPic} alt="Andres Agudelo" className="h-full w-full object-cover" />
+          </article>
+
+          <article className="mx-auto flex max-h-[350px] w-full items-center rounded-2xl bg-gray-400/15 p-6 md:col-span-2 lg:text-lg">
+            <p>
+              When I&apos;m not immersed in the screen, you&apos;ll often find me planning my next
+              adventure and enjoying the excitement of exploring new places. Traveling is my way of
+              seeing the world and collecting cool stories along the way.
+            </p>
+          </article>
+
+          <article className="mx-auto flex max-h-[350px] w-full items-center rounded-2xl bg-gray-400/15 p-6 md:col-span-2 lg:text-lg">
+            <p>
+              I also love to hit the gym or box, I&apos;m constantly challenging myself and seeing
+              what I can do. When it&apos;s time to chill, I&apos;m all about hanging out with
+              friends, gaming or just vibing to music, These are the things that keep me going.
+            </p>
+          </article>
+
+          <article className="relative mx-auto max-h-[350px] w-full overflow-clip rounded-2xl bg-gray-400/15 md:col-span-1 lg:col-span-1">
+            <Image
+              src={map}
+              alt="Andres Agudelo"
+              className="h-full w-full object-cover object-right-top"
+            />
+            <div className="absolute left-1/2 top-1/2 size-20 -translate-x-1/2 -translate-y-1/2 rounded-full after:absolute after:inset-0 after:rounded-full after:outline after:outline-2 after:outline-offset-2 after:outline-gray-950/30 after:content-['']">
+              <div className="absolute inset-0 z-[-2] animate-ping rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 [animation-duration:2s]"></div>
+              <div className="absolute inset-0 z-[-1] rounded-full bg-gradient-to-r from-emerald-300 to-sky-400"></div>
+              <Image src={memojiThumb} alt="Andres Agudelo" className="size-20" />
+            </div>
+          </article>
+
+          <article className="relative mx-auto hidden w-full overflow-clip rounded-2xl bg-gray-400/15 lg:block">
+            <Image
+              src={amsterdamPic}
+              alt="Andres Agudelo"
+              className="h-full w-full object-cover transition duration-300 hover:scale-125"
+            />
+          </article>
+
+          <article className="relative mx-auto hidden w-full overflow-clip rounded-2xl bg-gray-400/15 lg:block">
+            <Image
+              src={ibizaPic}
+              alt="Andres Agudelo"
+              className="h-full w-full object-cover transition duration-300 hover:scale-125"
+            />
+          </article>
+
+          <article className="relative mx-auto hidden w-full overflow-clip rounded-2xl bg-gray-400/15 lg:block">
+            <Image
+              src={ghentPic}
+              alt="Andres Agudelo"
+              className="h-full w-full object-cover transition duration-300 hover:scale-125"
+            />
+          </article>
+        </div>
+      </section>
     </main>
   )
 }
